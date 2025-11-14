d7e52e7b3df808b4ebc638f2f9c09526c0ba820e
James Ward <james@jamesward.com>
2025-11-14 11:53:16 -0700

    cleanup

--- src/main/java/com/embabel/prepper/agent/ContactRepository.java
+++ src/main/java/com/embabel/prepper/agent/ContactRepository.java
@@ -3,12 +3,5 @@ package com.embabel.prepper.agent;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface ContactRepository extends ListCrudRepository<Domain.Contact, Long> {
-
-    Optional<Domain.Contact> findByEmail(String email);
-
-    boolean existsByEmail(String email);
-}
+public interface ContactRepository extends ListCrudRepository<Domain.Contact, Long> { }
